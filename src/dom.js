cf2d49b4a6d5172c00f2438d6d0bbcac585d18a2
jianglei <1750644780@qq.com>
2021-05-22 12:27:20 +0800

    init

--- src/dom.js
+++ src/dom.js
@@ -1,14 +1,24 @@
 window.dom = {
-    create(){}
-};
-window.dom = {
-    find(){
-        你来补全
+    find(selector, scope) {
+        return (scope || document).querySelectorAll(selector)
     },
-    style(){
-        你来补全
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            node.style[name] = value
+        } else if (arguments.length === 2) {
+            if (typeof name === 'string') {
+                return node.style[name]
+            } else if (name instanceof Object) {
+                const object = name
+                for (let key in object) {
+                    node.style[key] = object[key]
+                }
+            }
+        }
     },
-    each(){
-        你来补全
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList[i])
+        }
     }
-}
+};
